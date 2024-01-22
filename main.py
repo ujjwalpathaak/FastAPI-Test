09896becffdabe2fd3b2ae83743518056155b6bd
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2024-01-22 11:30:09 +0530

    c

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/items/")
+@app.post("/items")
 async def create_item(item: dict):
-    print(item)
+    print(item.job_id)
+    print(item.predictions[0].results.predictions)
